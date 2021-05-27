6e0c56283d3858fe11210cd4ae0462be4b0e658f
Sizan-Aminun <71691929+sizanaminun@users.noreply.github.com>
2021-05-28 01:28:02 +0600

    t

---
+++ main.js
@@ -0,0 +1,31 @@
+// function handleClick(){
+//     document.body.style.color ='blue';
+// }
+
+// const myForm = document.getElementById("myForm")
+// myForm.addEventListener("click",handleClick);
+//     // e.preventDefault();
+//     console.log("submitted");
+
+
+
+
+// let btnShow =document.querySelector('button');
+// let input = document.querySelector('myForm');
+// let output = document.querySelector('h1');
+
+// btnShow.addEventListener('click',() =>{
+//     output.innerText = myForm.value;
+// });
+// input.addEventListener('keyup',() =>{
+//     if(myForm.value.length >6)btnShow.disabled = true
+//     else btnShow.disabled = false
+// })
+
+
+
+
+// const myForm = document.getElementById("myForm")
+// myForm.disabled = true;
+
+
