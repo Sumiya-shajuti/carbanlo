12a047b966a40e230e7c62408a31091d2d99fedf
Sizan-Aminun <71691929+sizanaminun@users.noreply.github.com>
2021-05-28 17:58:21 +0600

    done

--- main.js
+++ main.js
@@ -1,31 +1,66 @@
-// function handleClick(){
-//     document.body.style.color ='blue';
-// }
-
-// const myForm = document.getElementById("myForm")
-// myForm.addEventListener("click",handleClick);
-//     // e.preventDefault();
-//     console.log("submitted");
+const table = document.getElementById('table')
+const final = document.getElementById('final')
 
 
+table.addEventListener('click', (e) => {
+
+    if (e.target.className.indexOf('active') === -1) {
+        e.target.className += ' active';
+    }
+
+    const one = document.getElementById('one').className.includes('active')
+    const two = document.getElementById('two').className.includes('active')
+    const three = document.getElementById('three').className.includes('active')
+    const four = document.getElementById('four').className.includes('active')
+    const five = document.getElementById('five').className.includes('active')
+    const six = document.getElementById('six').className.includes('active')
+    const seven = document.getElementById('seven').className.includes('active')
+    const eight = document.getElementById('eight').className.includes('active')
+    const nine = document.getElementById('nine').className.includes('active')
+    const ten = document.getElementById('ten').className.includes('active')
+
+    if (e.target.id === 'one') {
+        document.getElementById('two').classList.remove('active')
+  
+    }
+    else if (e.target.id === 'two') {
+        document.getElementById('one').classList.remove('active')
+    }
+
+    if (e.target.id === 'three') {
+        document.getElementById('four').classList.remove('active')
+    }
+    else if (e.target.id === 'four') {
+        document.getElementById('three').classList.remove('active')
+    }
+
+    if (e.target.id === 'five') {
+        document.getElementById('six').classList.remove('active')
+    }
+    else if (e.target.id === 'six') {
+        document.getElementById('five').classList.remove('active')
+    }
+
+    if (e.target.id === 'seven') {
+        document.getElementById('eight').classList.remove('active')
+    }
+    else if (e.target.id === 'eight') {
+        document.getElementById('seven').classList.remove('active')
+    }
+
+    if (e.target.id === 'nine') {
+        document.getElementById('ten').classList.remove('active')
+    }
+    else if (e.target.id === 'ten') {
+        document.getElementById('nine').classList.remove('active')
+    }
 
 
-// let btnShow =document.querySelector('button');
-// let input = document.querySelector('myForm');
-// let output = document.querySelector('h1');
+    if ((one || two) && (three || four) && (five || six) && (seven || eight) && (nine || ten)) {
+        final.disabled = false;
+    }
+    else {
+        final.disabled = true;
+    }
 
-// btnShow.addEventListener('click',() =>{
-//     output.innerText = myForm.value;
-// });
-// input.addEventListener('keyup',() =>{
-//     if(myForm.value.length >6)btnShow.disabled = true
-//     else btnShow.disabled = false
-// })
-
-
-
-
-// const myForm = document.getElementById("myForm")
-// myForm.disabled = true;
-
-
+})
